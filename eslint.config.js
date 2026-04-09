6c2599e173164c9fc116c7bfa74b6d82847c4f37
Oscar Falkman <oscar.falkman@sr.se>
2026-04-09 19:53:06 +0200

    chore: update eslint config with node env

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,16 @@
 import eslint from "@eslint/js";
+import globals from 'globals';
 
 export default [
   {
     ignores: ["**/node_modules/**", "html/**"],
   },
   eslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.nodeBuiltin
+      }
+    }
+  },
 ];
